c9737dd844177d26751799b52801711b9e3e12b6
Fusion <115903041+TheOneGaming@users.noreply.github.com>
2024-11-07 21:27:26 -0500

    Update CelestialScript.cs
    Changed some things around to utilize the new CoordSpacePosition class and CoordinateSpace type

--- CelestialScript.cs
+++ CelestialScript.cs
@@ -1,10 +1,11 @@
 using Godot;
 using System;
+using CustomTypes;
 
 public partial class CelestialScript : StaticBody3D
 {
 	[Export] public float Mass;
-	[Export] public CelestialScript ReferenceBody;
+	[Export] public CelestialScript ParentCelestial;
 	[Export] public float a;
 	private float b;
 	[Export] public float e;
@@ -15,49 +16,29 @@ public partial class CelestialScript : StaticBody3D
 	private float tol = 0.00001f;
 	private float E;
 	[Export] public float Radius;
-	[Export] public int CoordLayer;
-	public Vector3 LocalPos;
+	[Export] public float SOIRadius;
+	[Export] public CoordinateSpace CoordLayer;
+	public CoordSpacePosition CoordPositions;
 
 	public override void _Ready()
 	{
 		b = a * (1 - Mathf.Pow(e, 2));
-		if (ReferenceBody != null)
+		if (ParentCelestial != null)
 		{
-			n = Mathf.Sqrt(GlobalValues.G * ReferenceBody.Mass /( a * GlobalValues.GetRefConversionFactor(CoordLayer, 0))) / (a * GlobalValues.GetRefConversionFactor(CoordLayer, 0)); // Don't forget all angles are in radians
+			n = Mathf.Sqrt(GlobalValues.G * ParentCelestial.Mass /( a * GlobalValues.GetRefConversionFactor(CoordLayer, 0))) / (a * GlobalValues.GetRefConversionFactor(CoordLayer, 0)); // Don't forget all angles are in radians
+			SOIRadius = a * Mathf.Pow(Mass / ParentCelestial.Mass, 0.4f);
 		}
 		else
 		{
-			LocalPos = Transform.Origin;
+			CoordPositions = new CoordSpacePosition(Transform.Origin);
 		}
 		Transform = Transform.Scaled(new Vector3(Radius, Radius, Radius));
+		GlobalValues.RecieveCelestials(this);
 	}
-	public Vector3 GetPosition(int Layer)
-	{
-		// Layer 1 is GalaxySpace, 2 is StarSpace, 3 is PlanetSpace, and 4+ are levels of nested PlanetSpace. To get the position in Layer 0 (RenderSpace), Use the "GetRenderSpacePos()" Method
-		if (Layer == CoordLayer)
-		{
-			return LocalPos;
-		}
-		else if (Layer < CoordLayer)
-		{
-			return ReferenceBody.GetPosition(Layer);
-		}
-		else
-		{
-			return new Vector3();
-		}
-	}
-	public Vector3 GetRenderSpacePos()
-	{
-		Vector3 PosAbsolute = new Vector3();
-		PosAbsolute += GetPosition(3) * GlobalValues.GetRefConversionFactor(3, 0) * GlobalValues.Scale;
-		PosAbsolute += GetPosition(2) * GlobalValues.GetRefConversionFactor(2, 0) * GlobalValues.Scale;
-		PosAbsolute += GetPosition(1) * GlobalValues.GetRefConversionFactor(1, 0) * GlobalValues.Scale;
-		return PosAbsolute;
-	}
+
 	public override void _Process(double dt)
 	{
-		if (ReferenceBody != null)
+		if (ParentCelestial != null)
 		{
 			float E = CalculateEccentricAnomaly(-n * GlobalValues.time + w);
 			float x = a * (Mathf.Cos(E)) - a + b;
@@ -67,9 +48,9 @@ public partial class CelestialScript : StaticBody3D
 			float y_Rot = x * (Mathf.Sin(w) * Mathf.Sin(i)) + z * (Mathf.Cos(w) * Mathf.Sin(i));
 			float z_Rot = x * (Mathf.Cos(w) * Mathf.Sin(l) + Mathf.Sin(w) * Mathf.Cos(l) * Mathf.Cos(i)) + z * (-Mathf.Sin(w) * Mathf.Sin(l) + Mathf.Cos(w) * Mathf.Cos(l) * Mathf.Cos(i));
 
-			LocalPos = new Vector3(x_Rot, y_Rot, z_Rot);
+			CoordPositions = new CoordSpacePosition(new Vector3(x_Rot, y_Rot, z_Rot), ParentCelestial);
 		}
-		Transform = new Transform3D(Transform.Basis, GetRenderSpacePos());
+		Transform = new Transform3D(Transform.Basis, CoordPositions.GetPositionAtLayer(CoordinateSpace.RenderSpace));
 	}
 	float CalculateEccentricAnomaly(float M)
 	{
