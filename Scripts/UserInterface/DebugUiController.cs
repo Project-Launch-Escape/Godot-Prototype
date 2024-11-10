a59900cb4f67e044afe00c17e4fc0cdc52dc675d
Fusion <115903041+TheOneGaming@users.noreply.github.com>
2024-11-10 18:52:36 -0500

    Organize code and add Camera speed readout

--- Scripts/UserInterface/DebugUiController.cs
+++ Scripts/UserInterface/DebugUiController.cs
@@ -6,11 +6,12 @@ namespace GodotPrototype.Scripts.UserInterface;
 public partial class DebugUiController : Node
 {
 	[ExportCategory("UI References")] 
+	[Export] public Label FPS;
+	[Export] public Label Paused;
 	[Export] public Label CurrentTime;
 	[Export] public Label CurrentRate;
-	[Export] public Label Paused;
+	[Export] public Label CurrentSpeed;
 	[Export] public Label CurrentSOIs;
-	[Export] public Label FPS;
 	[Export] public Label DistanceToCelestial;
 
 	private const double Minute = 60;
@@ -18,31 +19,24 @@ public partial class DebugUiController : Node
 	private const double Day = Hour * 24;
 	private const double Year = Day * 365.2422d;
 
-	private int celestialIndex;
-	
+	private int _celestialIndex;
+	private float[] _fPSPrev = new float[120];
+	private int _frame;
 	public override void _Process(double delta)
 	{
+		UpdateFPS(delta);
+		Paused.Text = $"Paused: {GlobalValues.Paused}";
 		UpdateCurrentTime(GlobalValues.Time);
 		UpdateTimeScale(GlobalValues.TimeScale);
-		UpdateFPS(delta);
+		UpdateSpeed(Freecam.VelocityMultiplier);
 		UpdateDistance();
-		Paused.Text = $"Paused: {GlobalValues.Paused}";
 	}
-
-	public void UpdateSOIs(List<CelestialScript> currentSOIs)
-	{
-		string sOIText = "Current SOIs: ";
-		for (int i = 0; i < currentSOIs.Count; i++)
-		{
-			sOIText += currentSOIs[i].Name + " ";
-		}
-
-		CurrentSOIs.Text = sOIText;
-	}
-
+	
 	private void UpdateFPS(double delta)
 	{
-		FPS.Text = $"FPS: {1 / delta}";
+		_fPSPrev[_frame] = (float)(1 / delta);
+		FPS.Text = $"FPS: {_fPSPrev.Average():F1}";
+		_frame = (_frame + 1) % _fPSPrev.Length;
 	}
 	
 	private void UpdateCurrentTime(double time)
@@ -51,7 +45,7 @@ public partial class DebugUiController : Node
 		var years = (long)((time - days) / Year);
 		CurrentTime.Text = $"Current Time: {years} yr, {days/Day:F1} d";
 	}
-
+	
 	private void UpdateTimeScale(double scale)
 	{
 		if (scale < Minute)
@@ -72,25 +66,41 @@ public partial class DebugUiController : Node
 			CurrentRate.Text = $"Current Rate: {scale/Year:F1} yr/s";
 		}
 	}
+	
+	private void UpdateSpeed(float speed)
+	{
+		CurrentSpeed.Text = $"Current Speed: {speed/1000:F1} km/s";
+	}
+	
+	public void UpdateSOIs(List<CelestialScript> currentSOIs)
+	{
+		var sOIText = "Current SOIs: ";
+		for (int i = 0; i < currentSOIs.Count; i++)
+		{
+			sOIText += currentSOIs[i].Name + ", ";
+		}
+
+		CurrentSOIs.Text = sOIText;
+	}
 
 	private void UpdateDistance()
 	{
-		CelestialScript targetCelestial = GlobalValues.AllCelestials[celestialIndex];
-		float Dist = targetCelestial.NestedPos.LocalPosition.DistanceTo(NestedPosition.ConvertPositionReference(Freecam.NestedPos, targetCelestial.NestedPos) * targetCelestial.CoordLayer.GetConversionFactor(0));
-		DistanceToCelestial.Text = $"Distance to {targetCelestial.Name}: {Dist/1000}Km";
+		var targetCelestial = GlobalValues.AllCelestials[_celestialIndex];
+		var dist = targetCelestial.NestedPos.LocalPosition.DistanceTo(NestedPosition.ConvertPositionReference(Freecam.NestedPos, targetCelestial.NestedPos, CoordinateSpace.RenderSpace));
+		DistanceToCelestial.Text = $"Distance to {targetCelestial.Name}: {dist/1000}km";
 	}
 	public override void _Input(InputEvent @event)
 	{
 		if (@event is InputEventKey inputEventKey)
 		{
-			if (inputEventKey.Pressed && inputEventKey.Keycode == Key.Minus && celestialIndex > 0)
+			if (inputEventKey.Pressed && inputEventKey.Keycode == Key.Minus && _celestialIndex > 0)
 			{
-				celestialIndex--;
+				_celestialIndex--;
 			}
 			if (inputEventKey.Pressed && inputEventKey.Keycode == Key.Equal)
 			{
-				celestialIndex++;
-				celestialIndex %= GlobalValues.AllCelestials.Count;
+				_celestialIndex++;
+				_celestialIndex %= GlobalValues.AllCelestials.Count;
 			}
 		}
 	}
