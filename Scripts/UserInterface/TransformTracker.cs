be766fad4c05e50fef62f322f474aeb73cb1f7c5
Lexi <arallen3660@gmail.com>
2024-11-09 20:24:23 -0500

    Track planet positions with a silly crosshair

---
+++ Scripts/UserInterface/TransformTracker.cs
@@ -0,0 +1,29 @@
+﻿using Godot;
+
+namespace GodotPrototype.Scripts.UserInterface;
+
+public partial class TransformTracker : Control
+{
+    
+    [Export]
+    public Camera3D CameraPosition;
+    [Export]
+    public Label TransformName;
+
+    private Node3D _toTrack;
+
+    public override void _Ready()
+    {
+        
+        _toTrack = (Node3D)GetParent();
+        TransformName.Text = _toTrack.Name;
+    }
+
+    public override void _Process(double delta)
+    {
+        var behind = CameraPosition.IsPositionBehind(_toTrack.GlobalPosition);
+        Visible = !behind;
+        if (behind) return;
+        Position = CameraPosition.UnprojectPosition(_toTrack.GlobalPosition);
+    }
+}
