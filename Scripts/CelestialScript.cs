aaba0a1b94a6b1c2027bf6fc33d8c3a27a7e38ae
Fusion <115903041+TheOneGaming@users.noreply.github.com>
2024-11-10 00:54:27 -0500

    Made celestial scaling scale properly

--- Scripts/CelestialScript.cs
+++ Scripts/CelestialScript.cs
@@ -1,6 +1,5 @@
 using Godot;
 using GodotPrototype.Scripts.Simulation.ReferenceFrames;
-
 namespace GodotPrototype.Scripts;
 
 public partial class CelestialScript : StaticBody3D
@@ -20,35 +19,36 @@ public partial class CelestialScript : StaticBody3D
 	[Export] public float SOIRadius;
 	[Export] public CoordinateSpace CoordLayer;
 	public NestedPosition NestedPos;
-
 	public override void _Ready()
 	{
 		b = a * (1 - Mathf.Pow(e, 2));
 		if (ParentCelestial != null)
 		{
-			n = Mathf.Sqrt(GlobalValues.G * ParentCelestial.Mass /( a * GlobalValues.GetRefConversionFactor(CoordLayer, 0))) / (a * GlobalValues.GetRefConversionFactor(CoordLayer, 0)); // Don't forget all angles are in radians
+			if (n == 0)
+			{
+				n = Mathf.Sqrt(GlobalValues.G * ParentCelestial.Mass /( a * GlobalValues.GetRefConversionFactor(CoordLayer, 0))) / (a * GlobalValues.GetRefConversionFactor(CoordLayer, 0));
+			}
+			
 			SOIRadius = a * Mathf.Pow(Mass / ParentCelestial.Mass, 0.4f);
 		}
 		else
 		{
 			NestedPos = new NestedPosition(Transform.Origin);
 		}
-		Transform = Transform.Scaled(new Vector3(Radius, Radius, Radius));
+		Transform = Transform.Scaled(new Vector3(1,1,1) * Radius * CoordLayer.Increment().GetConversionFactor(CoordinateSpace.RenderSpace) * GlobalValues.Scale);
 		GlobalValues.ReceiveCelestials(this);
 	}
-
 	public override void _Process(double dt)
 	{
 		if (ParentCelestial != null)
 		{
-			float E = (float)CalculateEccentricAnomaly((-n * GlobalValues.Time + w) % Math.Tau);
+			float E = w + (float)CalculateEccentricAnomaly((-n * GlobalValues.Time + w) % Math.Tau);
 			float x = a * (Mathf.Cos(E)) - a + b;
 			float z = b * Mathf.Sin(E);
 
 			float x_Rot = x * (Mathf.Cos(w) * Mathf.Cos(l) - Mathf.Sin(w) * Mathf.Sin(l) * Mathf.Cos(i)) + z * (-Mathf.Sin(w) * Mathf.Cos(l) - Mathf.Cos(w) * Mathf.Sin(l) * Mathf.Cos(i));
 			float y_Rot = x * (Mathf.Sin(w) * Mathf.Sin(i)) + z * (Mathf.Cos(w) * Mathf.Sin(i));
 			float z_Rot = x * (Mathf.Cos(w) * Mathf.Sin(l) + Mathf.Sin(w) * Mathf.Cos(l) * Mathf.Cos(i)) + z * (-Mathf.Sin(w) * Mathf.Sin(l) + Mathf.Cos(w) * Mathf.Cos(l) * Mathf.Cos(i));
-
 			NestedPos = new NestedPosition(new Vector3(x_Rot, y_Rot, z_Rot), ParentCelestial);
 		}
 		Transform = new Transform3D(Transform.Basis, NestedPos.GetPositionAtLayer(CoordinateSpace.RenderSpace));
