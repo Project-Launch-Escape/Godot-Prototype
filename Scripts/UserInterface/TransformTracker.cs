ca0b0a6104c84e77359587ad6e4164fe5deeaa0b
Fusion <115903041+TheOneGaming@users.noreply.github.com>
2024-11-10 18:54:09 -0500

    Add toggling Trackers with 'M'
    Now you can admire the prettiness of the solar system without obstruction!

--- Scripts/UserInterface/TransformTracker.cs
+++ Scripts/UserInterface/TransformTracker.cs
@@ -1,29 +1,42 @@
-﻿using Godot;
+using Godot;
 
 namespace GodotPrototype.Scripts.UserInterface;
 
 public partial class TransformTracker : Control
 {
-    
+
     [Export]
     public Camera3D CameraPosition;
     [Export]
     public Label TransformName;
 
+    private bool _visible = true;
+        
     private Node3D _toTrack;
 
     public override void _Ready()
     {
-        
+
         _toTrack = (Node3D)GetParent();
         TransformName.Text = _toTrack.Name;
+
     }
 
     public override void _Process(double delta)
     {
         var behind = CameraPosition.IsPositionBehind(_toTrack.GlobalPosition);
-        Visible = !behind;
+        Visible = !behind && _visible;
         if (behind) return;
         Position = CameraPosition.UnprojectPosition(_toTrack.GlobalPosition);
+    }
+    public override void _Input(InputEvent @event)
+    {
+        if (@event is InputEventKey inputEventKey)
+        {
+            if (inputEventKey.Pressed && inputEventKey.Keycode == Key.M)
+            {
+                _visible = !_visible;
+            }
+        }
     }
 }
