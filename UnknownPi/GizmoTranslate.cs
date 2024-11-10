e859dfc25f69bef3d385fc78a532d5f0f1f2734a
UknownPI <lepro.pablo@gmail.com>
2024-11-10 18:33:16 +0100

    Gizmos
    Translate Gizmos (+a little bit of other random stuff)

---
+++ UnknownPi/GizmoTranslate.cs
@@ -0,0 +1,94 @@
+using Godot;
+using System;
+
+public partial class GizmoTranslate : Node3D
+{
+	private const float RayLength = 1000.0f;
+	private int ispressed = -1;
+	private bool status =  false;
+	private Node collider;
+	private uint mask = 0b00000000_00000000_00000000_00000001;
+	public override void _Ready()
+	{
+		this.Hide();
+	}
+	public override void _Process(double delta)
+	{		
+	}
+	
+	public override void _Input(InputEvent @event)
+	{
+	if (@event is InputEventMouseButton eventMouseButton && eventMouseButton.Pressed && eventMouseButton.ButtonIndex == MouseButton.Left)
+	{
+		var camera3D = (Camera3D)(GetViewport().GetCamera3D());
+		var from = camera3D.ProjectRayOrigin(eventMouseButton.Position);
+		var to = from + camera3D.ProjectRayNormal(eventMouseButton.Position) * RayLength;
+		var spaceState = GetWorld3D().DirectSpaceState;
+		
+		var query = PhysicsRayQueryParameters3D.Create(from, to);
+		query.CollideWithAreas = true;
+		query.CollisionMask = mask;
+		var result = GetWorld3D().DirectSpaceState.IntersectRay(query);
+		if (result.Count==0) {this.Hide();mask = 0b00001;return;}
+		if (mask != 0b10000){mask = 0b10000;collider = (Node)result["collider"];this.Show();this.Position = ((Node3D)collider).GlobalPosition;return;}
+		
+		if ((Node)result["collider"] is StaticBody3D){
+			
+			StaticBody3D res = (StaticBody3D)result["collider"];
+			
+			Node3D p = (Node3D)res.GetParent();
+			switch (res.Name)
+			{
+			case "RED"  :ispressed = 0;break;
+			case "GREEN":ispressed = 1;break;
+			case "BLUE" :ispressed = 2;break;
+			}
+			
+		}
+	} else if (@event is InputEventMouseButton buttonEvent && buttonEvent.ButtonIndex == MouseButton.Left && !buttonEvent.Pressed)
+		{
+			ispressed = -1;
+		}
+		if (@event is InputEventMouse eve)
+	{
+		if (ispressed != -1){
+			Node3D Gizmos = this;//((Node3D)GetNode("../GizmoTranslate"));
+			Camera3D Camera = (Camera3D)(GetViewport().GetCamera3D());
+			
+			Vector3 A = new Vector3(0,0,0);
+			if (ispressed == 0){ A = Vector3.Right;} // Red
+			if (ispressed == 1){ A = Vector3.Up;} // Green
+			if (ispressed == 2){ A = Vector3.Back;} // Blue
+			Vector3 G = Gizmos.Position;
+			Vector3 R = G+A;
+			
+			Vector3 C = Camera.GlobalPosition;
+			C = C.Rotated(Vector3.Right, Mathf.Pi / 2);
+			// The Plan formed by GRC is the one is which the gizmo is and is facing camera
+			Vector3 N = (C-G).Normalized(); // Normal Vector of the plane,
+			
+			StaticBody3D plane = ((StaticBody3D)Gizmos.GetChild(ispressed));
+			plane.LookAt(Gizmos.Position+Camera.ProjectRayNormal(eve.Position).Normalized(),A);
+			Vector3 r = plane.Rotation * A;
+			plane.Rotation = r;
+			
+			var from = Camera.ProjectRayOrigin(eve.Position);
+			var to = from + Camera.ProjectRayNormal(eve.Position) * RayLength;
+			var spaceState = GetWorld3D().DirectSpaceState;
+			var query = PhysicsRayQueryParameters3D.Create(from, to);
+			query.CollideWithBodies =true;
+			if (ispressed == 0){query.CollisionMask =  0b0010; }//0b00000000_00000000_00000000_00000010
+			if (ispressed == 1){query.CollisionMask =  0b0100; } // Raycast Mask , so that it only collides with the right plane
+			if (ispressed == 2){query.CollisionMask =  0b1000; }
+			var result = GetWorld3D().DirectSpaceState.IntersectRay(query);
+			if (((StaticBody3D)result["collider"]).Name=="RedPlane" || ((StaticBody3D)result["collider"]).Name=="GreenPlane" || ((StaticBody3D)result["collider"]).Name=="BluePlane"){
+				Vector3 p = (Vector3)result["position"]-Gizmos.Position; // Change Of Position
+				p*=A; // Only Change Position On Axis
+				Gizmos.Position += p ; // Apply Change of Position
+				((Node3D)collider).GlobalPosition = Gizmos.Position;
+			}
+		}
+		
+		}
+}
+}
