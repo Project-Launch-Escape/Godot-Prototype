0d02608fa3b48fc5741a40334c1705fa76460fa2
Fusion <115903041+TheOneGaming@users.noreply.github.com>
2024-11-13 22:00:53 -0500

    Refactored to use a method specifically for calculating the position
    To allow other scripts to easily get the position of the celestial at a given value of E (For orbit rendering)

--- Scripts/CelestialScript.cs
+++ Scripts/CelestialScript.cs
@@ -19,6 +19,8 @@ public partial class CelestialScript : StaticBody3D
 	[Export] public float SOIRadius;
 	[Export] public CoordinateSpace CoordLayer;
 	public NestedPosition NestedPos;
+	[Export] public Color CelestialColor;
+
 	public override void _Ready()
 	{
 		b = a * (1 - Mathf.Pow(e, 2));
@@ -42,22 +44,27 @@ public partial class CelestialScript : StaticBody3D
 	{
 		if (ParentCelestial != null)
 		{
-			float E = w + (float)CalculateEccentricAnomaly((-n * GlobalValues.Time + w) % Math.Tau);
-			float x = a * (Mathf.Cos(E)) - a + b;
-			float z = b * Mathf.Sin(E);
-
-			float x_Rot = x * (Mathf.Cos(w) * Mathf.Cos(l) - Mathf.Sin(w) * Mathf.Sin(l) * Mathf.Cos(i)) + z * (-Mathf.Sin(w) * Mathf.Cos(l) - Mathf.Cos(w) * Mathf.Sin(l) * Mathf.Cos(i));
-			float y_Rot = x * (Mathf.Sin(w) * Mathf.Sin(i)) + z * (Mathf.Cos(w) * Mathf.Sin(i));
-			float z_Rot = x * (Mathf.Cos(w) * Mathf.Sin(l) + Mathf.Sin(w) * Mathf.Cos(l) * Mathf.Cos(i)) + z * (-Mathf.Sin(w) * Mathf.Sin(l) + Mathf.Cos(w) * Mathf.Cos(l) * Mathf.Cos(i));
-			NestedPos = new NestedPosition(new Vector3(x_Rot, y_Rot, z_Rot), ParentCelestial);
+			CalculateEccentricAnomaly((n * GlobalValues.Time + w) % Math.Tau);
+			NestedPos = new NestedPosition(GetPositionAtE(E), ParentCelestial);
 		}
 		Transform = new Transform3D(Transform.Basis, NestedPos.GetPositionAtLayer(CoordinateSpace.RenderSpace));
 	}
-	double CalculateEccentricAnomaly(double M)
+
+	public Vector3 GetPositionAtE(double E)
+	{
+		var x = a * Mathf.Cos(E) - a + b;
+		var z = b * Mathf.Sin(E);
+
+		var x_Rot = x * (Mathf.Cos(w) * Mathf.Cos(l) - Mathf.Sin(w) * Mathf.Sin(l) * Mathf.Cos(i)) + z * (-Mathf.Sin(w) * Mathf.Cos(l) - Mathf.Cos(w) * Mathf.Sin(l) * Mathf.Cos(i));
+		var y_Rot = x * (Mathf.Sin(w) * Mathf.Sin(i)) + z * (Mathf.Cos(w) * Mathf.Sin(i));
+		var z_Rot = x * (Mathf.Cos(w) * Mathf.Sin(l) + Mathf.Sin(w) * Mathf.Cos(l) * Mathf.Cos(i)) + z * (-Mathf.Sin(w) * Mathf.Sin(l) + Mathf.Cos(w) * Mathf.Cos(l) * Mathf.Cos(i));
+		return new Vector3((float)x_Rot, (float)y_Rot, (float)z_Rot);
+	}
+	private double CalculateEccentricAnomaly(double M)
 	{
 		for (int i = 0; i < 10; i++)
 		{
-			double dE = (E - e * Mathf.Sin(E) - M) / (1 - e * Mathf.Cos(E));
+			var dE = (E - e * Mathf.Sin(E) - M) / (1 - e * Mathf.Cos(E));
 			E -= dE;
 			if (Mathf.Abs(dE) < tol)
 			{
