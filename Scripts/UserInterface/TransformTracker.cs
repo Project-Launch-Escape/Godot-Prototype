7ef9cac33e86f370a93f81e2ee7c8c49713f4090
Fusion <115903041+TheOneGaming@users.noreply.github.com>
2024-11-15 19:33:24 -0500

    Added scaling of trackers depending on Coordinatespace
    This way, the trackers of moons don't obscure those of their host planets.

--- Scripts/UserInterface/TransformTracker.cs
+++ Scripts/UserInterface/TransformTracker.cs
@@ -5,38 +5,39 @@ namespace GodotPrototype.Scripts.UserInterface;
 public partial class TransformTracker : Control
 {
 
-    [Export]
-    public Camera3D CameraPosition;
-    [Export]
-    public Label TransformName;
+	[Export]
+	public Camera3D CameraPosition;
+	[Export]
+	public Label TransformName;
 
-    private bool _visible = true;
-        
-    private Node3D _toTrack;
+	private bool _visible = true;
+		
+	private Node3D _toTrack;
+	
+	private static float[] _scaleAtCoordLayer = [0.01f, 1.125f, 1f, 0.7f];
+	public override void _Ready()
+	{
 
-    public override void _Ready()
-    {
+		_toTrack = (Node3D)GetParent();
+		TransformName.Text = _toTrack.Name;
+		Scale = new Vector2(1f,1f) * _scaleAtCoordLayer[(int)((CelestialScript)_toTrack).CoordLayer];
+	}
 
-        _toTrack = (Node3D)GetParent();
-        TransformName.Text = _toTrack.Name;
-
-    }
-
-    public override void _Process(double delta)
-    {
-        var behind = CameraPosition.IsPositionBehind(_toTrack.GlobalPosition);
-        Visible = !behind && _visible;
-        if (behind) return;
-        Position = CameraPosition.UnprojectPosition(_toTrack.GlobalPosition);
-    }
-    public override void _Input(InputEvent @event)
-    {
-        if (@event is InputEventKey inputEventKey)
-        {
-            if (inputEventKey.Pressed && inputEventKey.Keycode == Key.M)
-            {
-                _visible = !_visible;
-            }
-        }
-    }
+	public override void _Process(double delta)
+	{
+		var behind = CameraPosition.IsPositionBehind(_toTrack.GlobalPosition);
+		Visible = !behind && _visible;
+		if (behind) return;
+		Position = CameraPosition.UnprojectPosition(_toTrack.GlobalPosition);
+	}
+	public override void _Input(InputEvent @event)
+	{
+		if (@event is InputEventKey inputEventKey)
+		{
+			if (inputEventKey.Pressed && inputEventKey.Keycode == Key.M)
+			{
+				_visible = !_visible;
+			}
+		}
+	}
 }
