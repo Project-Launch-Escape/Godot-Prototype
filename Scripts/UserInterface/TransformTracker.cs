2f47f90003d19d4c78868de2850916834dada823
Fusion <115903041+TheOneGaming@users.noreply.github.com>
2024-12-29 19:27:27 -0500

    Updated to have the label set in the editor and to use the new global UIVisible bool

--- Scripts/UserInterface/TransformTracker.cs
+++ Scripts/UserInterface/TransformTracker.cs
@@ -5,41 +5,26 @@ namespace GodotPrototype.Scripts.UserInterface;
 public partial class TransformTracker : Control
 {
 
-	private Camera3D _camera;
-	private Label _transformName;
+	public static Camera3D Camera;
+	[Export] private Label _transformName;
 
-	private bool _visible = true;
-		
 	private Node3D _toTrack;
 	
-	private static readonly float[] ScaleAtCoordLayer = [0.01f, 1.125f, 1f, 0.7f];
+	private static readonly float[] ScaleAtCoordLayer = [1f, 1.125f, 1f, 0.7f];
 	
 	public override void _Ready()
 	{
-		_camera = (Camera3D)GetNode("/root/GameScene/Camera");
-		_transformName = (Label)FindChild("Label");
-		
 		_toTrack = (Node3D)GetParent();
 		_transformName.Text = _toTrack.Name;
 
-		Scale = new Vector2(1f,1f) * ScaleAtCoordLayer[(int)((CelestialScript)_toTrack).NestedPos.CoordLayer];
+		Scale = Vector2.One * ScaleAtCoordLayer[(int)((Celestial)_toTrack).NestedPos.CoordLayer];
 	}
 
 	public override void _Process(double delta)
 	{
-		var behind = _camera.IsPositionBehind(_toTrack.GlobalPosition);
-		Visible = !behind && _visible;
+		var behind = Camera.IsPositionBehind(_toTrack.GlobalPosition);
+		Visible = !behind && GlobalValues.UIVisible;
 		if (behind) return;
-		Position = _camera.UnprojectPosition(_toTrack.GlobalPosition);
-	}
-	public override void _Input(InputEvent @event)
-	{
-		if (@event is InputEventKey inputEventKey)
-		{
-			if (inputEventKey.Pressed && inputEventKey.Keycode == Key.M)
-			{
-				_visible = !_visible;
-			}
-		}
+		Position = Camera.UnprojectPosition(_toTrack.GlobalPosition);
 	}
 }
