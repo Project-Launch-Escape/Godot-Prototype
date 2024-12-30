a7a2f28109eb25e336294f9503c2d588c7382631
Fusion <115903041+TheOneGaming@users.noreply.github.com>
2024-12-29 19:48:33 -0500

    Moved some things from elsewhere into GlobalValues for convenience.
    I moved the Time Unit constants and converting from Time to a string from the DebugUI, along with the UI Visibility toggle into this class for convenient access and stuff.

--- Scripts/GlobalValues.cs
+++ Scripts/GlobalValues.cs
@@ -1,4 +1,6 @@
 using Godot;
+using GodotPrototype.Scripts.Simulation.DoublePrecision;
+using GodotPrototype.Scripts.Simulation.Physics;
 using GodotPrototype.Scripts.Simulation.ReferenceFrames;
 namespace GodotPrototype.Scripts;
 
@@ -7,10 +9,16 @@ public partial class GlobalValues : Node
     public static double TimeScale = 864000;
     public static double Time;
     public const double G = 0.00000000006674315f;
-    public static readonly List<CelestialScript> AllCelestials = [];
+    public static readonly List<Celestial> AllCelestials = [];
     public static bool Paused;
+    public static bool UIVisible = true;
     
-    public static void ReceiveCelestials(CelestialScript celestial)
+    public const double Minute = 60;
+    public const double Hour = Minute * 60;
+    public const double Day = Hour * 24;
+    public const double Year = Day * 365.2422d;
+    
+    public static void ReceiveCelestials(Celestial celestial)
     {
         if (!AllCelestials.Contains(celestial))
         {
@@ -23,22 +31,35 @@ public partial class GlobalValues : Node
         if (Paused) return;
         Time += delta * TimeScale;
     }
+
+    public static string TimeToYearDayString(double time)
+    {
+        var days = time % Year;
+        var years = (long)((time - days) / Year);
+        return $"{years} yr, {days/Day:F1} d";
+    }
     
     public override void _Input(InputEvent @event)
     {
-        if (@event is InputEventKey inputEventKey)
+        if (@event is InputEventKey inputEventKey && inputEventKey.Pressed)
         {
-            if (inputEventKey.Pressed && inputEventKey.Keycode == Key.Space)
+            switch (inputEventKey.Keycode)
             {
-                Paused = !Paused;
-            }
-            if (inputEventKey.Pressed && inputEventKey.Keycode == Key.Comma)
-            {
-                TimeScale /= 1.5f;
-            }
-            if (inputEventKey.Pressed && inputEventKey.Keycode == Key.Period)
-            {
-                TimeScale *= 1.5f;
+                case Key.Space:
+                    Paused = !Paused;
+                    break;
+                case Key.Comma:
+                    TimeScale /= 1.5;
+                    break;
+                case Key.Period:
+                    TimeScale *= 1.5;
+                    break;
+                case Key.Slash:
+                    TimeScale = 1;
+                    break;
+                case Key.M:
+                    UIVisible = !UIVisible;
+                    break;
             }
         }
     }
